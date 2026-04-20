cb1cfb958ddf4058a4069e0be4fa371e82b895d5
Sultan Simsek <sultansimsek294@icloud.com>
2026-04-20 12:32:48 -0500

    Added records of films and actors

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,2 +1,16 @@
-/* Sakila is a database used to collect information about actors and films for use by keeping records of the actors and the films 
+/*
+Sakila is a database used to collect information about actors and films for use by keeping records of the actors and the films.
 */
+
+USE sakila;
+SELECT * 
+FROM actor;
+-- Retrieved 0 records
+
+SELECT * 
+FROM film;
+-- Retrieved 0 records
+
+
+
+
