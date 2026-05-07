d43df1203d06476f5a747606ea44c562896147b7
Sultan Simsek <sultansimsek294@icloud.com>
2026-05-07 18:30:12 -0500

    'Practicing print statement and updating variables'

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,17 @@
 print('Hello World!')
+
+message = 'Hello World!'
+print(message)
+#It prints twice cause we have two print statements. The first one prints the string directly, and the second one prints the value stored in the variable 'message', which is also 'Hello World!'.
+
+#Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+#It doesn't print $ sign nor the 0 at the end, it just treats them as nummeric values, python doesn't understand what I am trying to say.
+cents = cents + .25
+print (dollars + cents
+       )
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + ' ' + c_str)
