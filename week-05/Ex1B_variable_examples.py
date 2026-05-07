4238057869b3458ab7567457522c828221793581
Sultan Simsek <sultansimsek294@icloud.com>
2026-05-07 18:50:27 -0500

    'Practiced variables, and learned reserved words in Python.'

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,18 @@
+customer_id = '1122'
+customer_fname = 'Sultana'
+customer_lname = 'Bahar'
+customer_gender = 'Female' 
+customer_dob = '2001-02-26'
+dl_number = 'DL1234567890'
+auto_policy_no = 'AP1122'
+# I created variables for name seperate thinking that it would be more useful but I can also create one variable for the full name.
+# Also I created the auto policy number related to customer id, thinking I might need to infer the info from the customer id, or vice versa.
+my_name = 'Sultan Simsek'
+my_city = 'Ankara'
+my_country = 'Turkey'
+# There are 33 reserved words in Python 3. Below I will ad 5 of these with their definitions as examples. 
+# None: This is a special constant in Python that represents the absence of a value or a null value. It is often used to indicate that a variable has no value or to signify the end of a list or loop.
+# and: This is a logical operator in Python.
+# or: This is another logical operator in Python.
+# while: This is a control flow statement in Python that allows you to execute a block of code repeatedly as long as a certain condition is true.
+# False: This is a boolean value in Python that represents the opposite of True. It is often used in conditional statements and loops to control the flow of the program.
