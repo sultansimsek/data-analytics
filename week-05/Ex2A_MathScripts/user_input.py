e062587f05275745354ba44485b0303282d664a6
Sultan Simsek <sultansimsek294@icloud.com>
2026-05-07 21:53:23 -0500

    User input practice.

--- week-05/Ex2A_MathScripts/user_input.py
+++ week-05/Ex2A_MathScripts/user_input.py
@@ -1,2 +1,3 @@
 bank_bal = input("What is your current bank balance? ")
 print("Your bank balance is: " + bank_bal)
+# One pitfall for using input might be unexpected result, for example they can enter different dadta types which can result in error.
