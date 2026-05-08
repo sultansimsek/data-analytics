a0bd9a447969b29818b1d4db001002e563710e45
Sultan Simsek <sultansimsek294@icloud.com>
2026-05-07 21:17:17 -0500

    Practiced more math problems.

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,4 @@
+s_side = 2
+l_side = 26
+rectangular_area = s_side * l_side
+print("The area of the rectangle is: " + str(rectangular_area))
---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,6 @@
+assets = 100000
+debts = 23000
+net_worth = assets - debts
+print("Your total assets are: " + str(assets))
+print("Your total debts are: " + str(debts))
+print("Your net worth is: " + str(net_worth))
